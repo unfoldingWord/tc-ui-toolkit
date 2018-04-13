ebf7fdd72323e37000612e83b5e2ac83a158f447
mannycolon <colonmanuel7@gmail.com>
2018-04-13 13:22:08 -0400

    started initial setup of tc-ui-toolkit. (started the CheckInforCard Component)

---
+++ src/tc-ui-toolkit/src/CheckInfoCard/CheckInfoCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './checkInfoCard.style.css';
+
+const CheckInfoCard = ({
+  title,
+  phrase,
+  seeMoreLabel,
+  onSeeMoreClick,
+  showSeeMoreButton,
+}) => {
+  return (
+    <div className="checkInfo">
+      <div className="leftSide">
+        <div className="title">
+          {title}
+        </div>
+      </div>
+      <div className="rightSide">
+        <div className="phrase">
+          {phrase}
+        </div>
+        <div onClick={showSeeMoreButton ? null : onSeeMoreClick} className={showSeeMoreButton ? 'linkInactive' : 'linkActive'}>
+          {seeMoreLabel}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+CheckInfoCard.propTypes = {
+  phrase: PropTypes.string,
+  title: PropTypes.string,
+  onSeeMoreClick: PropTypes.func,
+  showSeeMoreButton: PropTypes.bool,
+}
+
+export default CheckInfoCard
