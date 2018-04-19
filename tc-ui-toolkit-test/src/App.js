e290a3d8c590dbe0bca0535f09e8d7710b2bd041
mannycolon <colonmanuel7@gmail.com>
2018-04-19 15:55:59 -0400

    Added react-bootstrap support

--- tc-ui-toolkit-test/src/App.js
+++ tc-ui-toolkit-test/src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 
-import { CheckInfoCard } from 'tc-ui-toolkit';
+import { CheckInfoCard, ScripturePane } from 'tc-ui-toolkit';
 
 class App extends Component {
   render() {
     return (
       <div style={{ padding: '10px' }}>
+        <ScripturePane
+          titleLabel="Step 1. Read"
+          expandedScripturePaneIconTitle="Click to show expanded resource panes"
+          openExpandedScripturePane={() => console.log('ExpandedScripturePane clicked')}
+        />
         <CheckInfoCard
           title="save, saves, saved, safe, salvation"
           phrase='The term "save" refers to keeping someone from experiencing something bad or harmful. To "be safe" means to be protected from harm or danger.'
           seeMoreLabel="See More"
           showSeeMoreButton={true}
-          onSeeMoreClick={() => console.log('clicked')}
+          onSeeMoreClick={() => console.log('CheckInfoCard clicked')}
         />
       </div>
     );
