5b16cc16552ee542dd43330951d39fb93f7f0af8
bspidel <bruce.spidel@gmail.com>
2018-04-20 16:05:29 -0400

    first commit of TranslationHelps

---
+++ src/TranslationHelps/TranslationHelps.js
@@ -0,0 +1,39 @@
+/* TranslationHelps is a feature of the TranslationWords tool and consists of:
+ *   A collapible sidebar showing the complete artical sumarized in panel
+ *     The sidebar has a sash that expands to about 20% of the application window
+ *     The sash can collapse to about 0.25in
+ *     The expanded sidebar vertically wraps article text and can scroll entire article
+ *     When expanded the "See More" button in panel is hidden 
+ *   It interoperates with CheckInfoCard
+ */ 
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Glyphicon} from 'react-bootstrap';
+import './TranslationHelps.styles.css';
+
+const TranslationHelps = ({
+  openExpandedHelpsPane,
+  expandedHelpsPaneIconTitle,
+  article 
+}) => {
+  return (
+    <div className="helps-sash-container">
+      <div className="helps-title-bar">
+        <Glyphicon
+          onClick={openExpandedHelpsPane}
+          glyph={"fullscreen"}
+          style={{ cursor: "pointer" }}
+          title={expandedHelpsPaneIconTitle}
+        />
+      </div>
+      <div className="helps"> 
+        {article}
+      </div>
+    </div>
+  )};
+
+  TranslationHelps.propTypes = {
+    article: PropTypes.object, 
+  }
+
+  export default TranslationHelps
