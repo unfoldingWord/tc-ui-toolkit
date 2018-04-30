0bc780efd6e0509ed44f5609dc3dd49d93ef77be
bspidel <bruce.spidel@gmail.com>
2018-04-30 14:20:24 -0400

    stash for another pr review

--- tc-ui-toolkit-test/src/App.js
+++ tc-ui-toolkit-test/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { CheckInfoCard, ScripturePane, TranslationHelps } from 'tc-ui-toolkit';
 
 const article = ( 
@@ -24,27 +25,45 @@ const article = (
 class App extends Component {
   render() {
     return (
-      <div style={{ padding: '10px' }}>
-        <ScripturePane
-          titleLabel="Step 1. Read"
-          closeButtonLabel="Close"
-          expandedScripturePaneTitle="Matthew"
-          expandButtonHoverText="Click to show expanded resource panes"
-        />
-        <CheckInfoCard
-          title="save, saves, saved, safe, salvation"
-          phrase='The term "save" refers to keeping someone from experiencing something bad or harmful. To "be safe" means to be protected from harm or danger.'
-          seeMoreLabel="See More"
-          showSeeMoreButton={true}
-          onSeeMoreClick={() => console.log('CheckInfoCard clicked')}
-          //onSeeMoreClick={() => TranslationHelps('Save') }
-        />
-        <TranslationHelps
-          article={article} 
-          expandedScripturePaneIconTitle="Click to show expanded help pane"
-          openExpandedScripturePane={() => console.log('Expanded translation helps clicked')}
-        />
-      </div>
+      <muiThemeProvider>
+        <div className='container'>
+          <div className='column1'>
+            menu column<br/>place holder
+          </div>
+
+          <div className='column2'>
+            <div className='scripture-item'>
+              <ScripturePane
+                titleLabel="Step 1. Read"
+                closeButtonLabel="Close"
+                expandedScripturePaneTitle="Matthew"
+                expandButtonHoverText="Click to show expanded resource panes"
+              />
+            </div>
+          
+            <div className='info-item'>
+              <CheckInfoCard
+                title="save, saves, saved, safe, salvation"
+                phrase='The term "save" refers to keeping someone from experiencing something bad or harmful. To "be safe" means to be protected from harm or danger.'
+                seeMoreLabel="See More"
+                showSeeMoreButton={true}
+                onSeeMoreClick={() => console.log('CheckInfoCard clicked')}
+                //onSeeMoreClick={() => TranslationHelps('Save') }
+              />
+            </div>
+          </div>
+
+          <div className='column3'>
+            <div className='helps-item'>
+              <TranslationHelps
+                article={article} 
+                expandedHelpsButtonHoverText="Click to show expanded help pane"
+                openExpandedHelpsPane={() => console.log('Expanded translation helps clicked')}
+              />
+            </div>
+          </div>
+        </div>
+      </muiThemeProvider>
     );
   }
 }
