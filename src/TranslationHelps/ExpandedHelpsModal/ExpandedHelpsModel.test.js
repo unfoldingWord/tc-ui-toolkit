13a46d0e9dbd839eb3d28351ec49f46ecd6c24c2
RoyalSix <jay.a.scottjr@biola.edu>
2018-05-14 17:29:27 -0700

    fixed tests

--- src/TranslationHelps/ExpandedHelpsModal/ExpandedHelpsModel.test.js
+++ src/TranslationHelps/ExpandedHelpsModal/ExpandedHelpsModel.test.js
@@ -1,23 +1,24 @@
 import React, {Component} from 'react'
-import renderer from 'react-test-renderer';
 import PropTypes from 'prop-types';
+import {MuiThemeProvider, createMuiTheme} from 'material-ui/styles';
+import {mount} from 'enzyme';
 
 import ExpandedHelpsModal from './';
 
 describe('Test ExpandedHelpsModal component', () => {
   test('Test initial display', () => {
-    const props = {
-      show: true
-    }
 
-    const tree = renderer
-      .create(
-        <div>
-          <ExpandedHelpsModal
-            show={false}
-          />
-        </div>)
-      .toJSON();
-    expect(tree).toEqual(<div><p>hello dialog</p></div>);
+    const theme = createMuiTheme({scrollbarThumb: {borderRadius: '10px'}});
+    const expectedTitle = 'Title';
+    const wrapper = mount(
+      <MuiThemeProvider theme={theme}>
+        <ExpandedHelpsModal
+          show={true}
+          onHide={() => {}}
+          title={expectedTitle}
+          article={'Test article'}
+        />
+      </MuiThemeProvider>)
+    expect(wrapper.find('.tool-bar-title').text(expectedTitle))
   });
 });
