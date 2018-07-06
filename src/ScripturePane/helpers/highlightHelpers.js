94307ad3741ef94228f4e0a5b903f285e61024bc
mannycolon <colonmanuel7@gmail.com>
2018-07-06 17:39:06 -0400

    moved console log up

--- src/ScripturePane/helpers/highlightHelpers.js
+++ src/ScripturePane/helpers/highlightHelpers.js
@@ -4,9 +4,9 @@ import { isWord, punctuationWordSpacing } from './stringHelpers';
 import { removeMarker } from './usfmHelpers';
 
 export function isWordArrayMatch(word, contextId) {
+  console.log(word);
   let isMatch = false;
   if (word && word.content && contextId && contextId.quote) {
-    console.log(word, word.content);
     isMatch = word.content.some(wordItem => {
       let foundMatch = false;
       if (contextId.quote.split(' ').includes(wordItem.content)) {
