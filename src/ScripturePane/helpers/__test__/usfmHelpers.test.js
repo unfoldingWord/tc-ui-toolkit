0151fc69938ca8db207dc12262c4d9ba3c49683d
Bruce McLean <mcleanb@photo-nomad.net>
2018-09-06 12:47:59 -0400

    Added unit tests for various USFM tags and problematic combinations.

--- src/ScripturePane/helpers/__test__/usfmHelpers.test.js
+++ src/ScripturePane/helpers/__test__/usfmHelpers.test.js
@@ -10,7 +10,7 @@ it('removes all extra tags', () => {
 
 it('removes f tags with following s5 tag', () => {
   const input = "He even tried to desecrate the temple, so we arrested him.\\f + \\ft Some ancient copies add, \\fqa \"We wanted to judge him according to our law \\fqa* . \\f*\\s5";
-  const expected = 'He even tried to desecrate the temple, so we arrested him.\\s5';
+  const expected = 'He even tried to desecrate the temple, so we arrested him.';
   const output = removeMarker(input);
   expect(output).toEqual(expected);
 });
@@ -24,19 +24,14 @@ it('removes f and s5 tags', () => {
 
 it('removes f tags with following p tag', () => {
   const input = "\\f + \\ft Acts 28:29—Some ancient copies have verse 29: \\fqa When he had said these things, the Jews went away. They were having a great dispute among themselves \\fqa* . \\f*\\p";
-  const expected = '\\p';
-  const output = removeMarker(input);
-  expect(output).toEqual(expected);
-});
-
-it('removes f and p tags', () => {
-  const input = "\\f + \\ft Acts 28:29—Some ancient copies have verse 29: \\fqa When he had said these things, the Jews went away. They were having a great dispute among themselves \\fqa* . \\f*\\p";
   const expected = '';
   const output = removeMarker(input);
   expect(output).toEqual(expected);
 });
 
-// it('cleans multiple usfm tags from 1jn1:4', () => {
-//   generateTest('filter/1jn1:4', ['f', 'q', 's5', 'p', 'z']);
-// });
-//
+it('removes f and pi tags', () => {
+  const input = "\\f + \\ft Acts 28:29—Some ancient copies have verse 29: \\fqa When he had said these things, the Jews went away. They were having a great dispute among themselves \\fqa* . \\f*\\pi more";
+  const expected = ' more';
+  const output = removeMarker(input);
+  expect(output).toEqual(expected);
+});
