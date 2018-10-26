600e0d67bfa575fbad2f3da51933826651cbf4ba
Richard Mahn <rich.mahn@unfoldingword.org>
2018-10-26 14:23:27 -0600

    Fixes morph

--- src/WordLexiconDetails/WordLexiconDetails.js
+++ src/WordLexiconDetails/WordLexiconDetails.js
@@ -19,7 +19,7 @@ class WordLexiconDetails extends React.Component {
 
     return (
       <div style={{ margin: '-10px 10px -20px', maxWidth: '400px' }}>
-        <span><strong>{translate('lemma')}</strong> {lemma}</span><br/>
+        <span><strong>{translate('noun')}</strong> {lemma}</span><br/>
         <span><strong>{translate('morphology')}</strong> {MorphUtils.getFullMorphologicalString(morph, translate)}</span><br/>
         <span><strong>{translate('strongs')}</strong> {strong}</span><br/>
         <span><strong>{translate('lexicon')}</strong> {lexicon}</span><br/>
