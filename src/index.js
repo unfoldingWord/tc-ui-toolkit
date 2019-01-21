7df0f1ae665ea27809f5f51cf60841dc558ae180
Joel Lonbeck <joel@neutrinographics.com>
2019-01-21 12:04:16 -0800

    exposed additional grouped menu helpers

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export { default as ScripturePane } from './ScripturePane';
 export { default as VerseEditor } from './VerseEditor';
 export { default as VerseCheck } from './VerseCheck';
 export { default as GroupMenu } from './GroupMenu';
-export { default as GroupedMenu } from './GroupedMenu';
+export { default as GroupedMenu, generateMenuItem, generateMenuData } from './GroupedMenu';
 export { default as Bookmark } from './Bookmark';
 export { default as WordLexiconDetails } from './WordLexiconDetails';
 exports.lexiconHelpers = require('./ScripturePane/helpers/lexiconHelpers');
