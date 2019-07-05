81f2ecdf6d35ae12ace87db11c399d931f91057b
mannycolon <colonmanuel7@gmail.com>
2019-07-05 17:48:10 -0400

    Moved the thelps article out of app.js to its own file.

---
+++ tc-ui-toolkit-test/src/assets/thelps.js
@@ -0,0 +1,29 @@
+export const article = `
+### Translation Strategies
+
+If the personification would be understood clearly, consider using it. If it would not be understood, here are some other ways for translating it.
+
+1. Add words or phrases to make it clear.
+1. Use words such as “like” or “as” to show that the sentences is not to be understood literally.
+1. Find a way to translate it without the personification.
+
+### Examples of Translation Strategies Applied
+
+1. Add words or phrases to make it clear.
+
+  * ** ... <u>sin crouches</u> at the door** (Genesis 4:7 ULT) - God speaks of sin as a wild animal that is waiting for the chance to attack.  This shows how dangerous sin is. An additional phrase can be added to make this danger clear.
+      * ... <u>sin</u> is at your door, <u>waiting to attack you</u>
+
+1. Use words such as “like” or “as” to show that the sentences is not to be understood literally.
+
+  * ** ... sin crouches at the door** (Genesis 4:7 ULT) - This can be translated with the word “as.”
+      * ... sin is crouching at the door, just <u>as a wild animal does waiting to attack a person</u>.
+
+1. Find a way to translate it without the personification.
+
+  * ** ... even the <u>winds and the sea obey him</u>**  (Matthew 8:27 ULT) - The men speak of the “wind and the sea as if they are able to hear” and obey Jesus as people can. This could also be translated without the idea of obedience by speaking of Jesus controlling them.
+      * He even <u>controls the winds and the sea</u>.
+
+
+**Note**: We have broadened our definition of “personification” to include “zoomorphism” (speaking of other things as if they had animal characteristics) and “anthropomorphism” (speaking of non-human things as if they had human characteristics.)
+`;
