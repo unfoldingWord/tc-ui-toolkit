7e661b383df081f22950a2b1736ae47f4c1d8834
Richard Mahn <rich.mahn@unfoldingword.org>
2019-08-06 06:53:06 -0400

    Revert some code

--- src/CheckInfoCard/PhraseWithTALinks.js
+++ src/CheckInfoCard/PhraseWithTALinks.js
@@ -1,46 +1,36 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class PhraseWithTALinks extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  onTALinkClick = (link) => {
-    window.followLink = link;
-  };
-
-  render() {
-    const {phrase} = this.props;
-    if (!phrase)
-      return phrase;
-    const regex = /\[\[rc:\/\/([^/]+\/ta\/man\/[^\]]+)]]/;
-    const splitPhrase = phrase.split(regex);
-    if (splitPhrase.length < 2)
-      return phrase;
-    console.log("split", splitPhrase);
-    const textAndLinks = splitPhrase
-      .reduce((prev, current, i) => {
-        if (!i)
-          return [current];
-        if (!(i % 2)) {
-          return prev.concat(current);
-        }
-        return prev.concat(
-          <a onClick={()=>this.onTALinkClick(current)}>
-            {current}
-          </a>
-        );
-      }, []);
-    console.log(textAndLinks);
-    return (<div>
-      {textAndLinks}
-    </div>);
-  }
+function PhraseWithTALinks({phrase}) {
+  if (!phrase)
+    return phrase;
+  const regex = /\[\[rc:\/\/([^/]+\/ta\/man\/[^\]]+)]]/;
+  const splitPhrase = phrase.split(regex);
+  if (splitPhrase.length < 2)
+    return phrase;
+  console.log("split", splitPhrase);
+  const textAndLinks = splitPhrase
+    .reduce((prev, current, i) => {
+      if (!i)
+        return [current];
+      if (!(i % 2)) {
+        return prev.concat(current);
+      }
+      return prev.concat(
+        <a onClick={() => onTALinkClick(current)}>
+          {current}
+        </a>
+      );
+    }, []);
+  console.log(textAndLinks);
+  return (<div>
+    {textAndLinks}
+  </div>);
 }
 
-PhraseWithTALinks.propTypes = {
-  phrase: PropTypes.string.isRequired,
-};
+function onTALinkClick(link) {
+  if (window.followLink)
+    window.followLink(link);
+}
 
 export default PhraseWithTALinks;
