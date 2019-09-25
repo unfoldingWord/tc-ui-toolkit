32e244e153eba8bfbc7a11f096ef4338b8851a5b
mannycolon <colonmanuel7@gmail.com>
2019-09-25 15:46:19 -0400

    logginf

--- src/VerseCheck/SaveArea/index.js
+++ src/VerseCheck/SaveArea/index.js
@@ -12,6 +12,12 @@ const SaveArea = ({
   handleGoToPrevious,
   handleOpenDialog,
 }) => {
+  console.log(
+    handleGoToNext,
+    handleGoToPrevious,
+    handleOpenDialog
+  );
+
   const handleNext = () => {
     selections.length > 0 || nothingToSelect ? handleGoToNext() : handleOpenDialog('next');
   };
