6a9a5031a50f9e965a63de5616386fa93bf0e216
Bruce McLean <mcleanb@photo-nomad.net>
2020-05-26 15:14:45 -0400

    working on SP title RTL

--- src/ScripturePane/Pane/index.js
+++ src/ScripturePane/Pane/index.js
@@ -9,6 +9,95 @@ import './Pane.styles.css';
 // components
 import Verse from '../Verse';
 
+// constants
+const PANECHAR = 9;
+
+/**
+ * get proper text arrangement
+ * @param {string} languageName
+ * @param {boolean} isLTR
+ * @param {string} bibleId
+ * @return {string}
+ */
+function getTargetBibleTitle(languageName, isLTR, bibleId) {
+  if (isLTR) {
+    return `${languageName} (${bibleId.toUpperCase()})`;
+  }
+  return `(${bibleId.toUpperCase()}) ${languageName}`;
+}
+
+/**
+ * create remove icon
+ * @param {function} clickToRemoveResourceLabel
+ * @param {number} index
+ * @param {function} removePane
+ * @return {*}
+ */
+function getRemoveicon(clickToRemoveResourceLabel, index, removePane) {
+  return <Glyphicon
+    className="remove-glyph-icon"
+    glyph={'remove'}
+    title={clickToRemoveResourceLabel}
+    onClick={() => removePane(index)}
+  />;
+}
+
+/**
+ * create title container with selected overall justification
+ * @param {boolean} isLTR - justification to use, if true do LTR
+ * @param {string} headingText
+ * @param {string} localizedDescription
+ * @return {*}
+ */
+function getTitleContainerContent(isLTR, headingText, localizedDescription) {
+  const styles = { textAlign: isLTR ? 'left' : 'right' };
+  return <div className="pane-title-container-content" style={styles}>
+    <span
+      className={headingText.length > 21 ? 'pane-title-text hint--bottom hint--medium' : 'pane-title-text'}
+      aria-label={headingText}>
+      {headingText.length > 21 ? headingText.slice(0, 21) + '...' : headingText}
+    </span>
+    <ContainerDimensions>
+      {
+        ({ width }) =>
+          <span
+            className={localizedDescription.length > width / PANECHAR ? 'pane-subtitle-text hint--bottom hint--medium' : 'pane-subtitle-text'}
+            aria-label={localizedDescription}>
+            {
+              localizedDescription.length > width / PANECHAR ?
+                localizedDescription.slice(0, Math.round(width / PANECHAR)) + '...' :
+                localizedDescription
+            }
+          </span>
+      }
+    </ContainerDimensions>
+  </div>;
+}
+
+/**
+ * create title container content with selected justification
+ * @param {boolean} isLTR - justification to use
+ * @param {string} headingText
+ * @param {string} localizedDescription
+ * @param {function} clickToRemoveResourceLabel
+ * @param {number} index
+ * @param {function} removePane
+ * @return {*}
+ */
+function getTitleContainer(isLTR, headingText, localizedDescription, clickToRemoveResourceLabel, index, removePane) {
+  if (isLTR) {
+    return <>
+      {getTitleContainerContent(isLTR, headingText, localizedDescription)}
+      {getRemoveicon(clickToRemoveResourceLabel, index, removePane)}
+    </>;
+  } else { // arrange rtl
+    return <>
+      {getRemoveicon(clickToRemoveResourceLabel, index, removePane)}
+      {getTitleContainerContent(isLTR, headingText, localizedDescription)}
+    </>;
+  }
+}
+
 const Pane = ({
   index,
   bibleId,
@@ -23,72 +112,16 @@ const Pane = ({
   clickToRemoveResourceLabel,
   translate,
 }) => {
-  const headingText = bibleId !== 'targetBible' ? languageName + ' (' + bibleId.toUpperCase() + ')' : (languageName || '');
-  const PANECHAR = 9;
+  const headingText = bibleId !== 'targetBible' ?
+    getTargetBibleTitle(languageName, layoutDirectionLTR, bibleId)
+    : (languageName || '');
   const localizedDescription = getTranslation(translate, `pane.${description}`, description);
-
-  function getGlyphicon() {
-    return <Glyphicon
-      className="remove-glyph-icon"
-      glyph={'remove'}
-      title={clickToRemoveResourceLabel}
-      onClick={() => removePane(index)}
-    />;
-  }
-
-  /**
-   * create title container with selected overall justification
-   * @param {boolean} ltr - justification to use
-   * @return {*}
-   */
-  function getTitleContainerContent(ltr) {
-    const styles = { textAlign: ltr ? 'left' : 'right' };
-    return <div className="pane-title-container-content" style={styles}>
-      <span
-        className={headingText.length > 21 ? 'pane-title-text hint--bottom hint--medium' : 'pane-title-text'}
-        aria-label={headingText}>
-        {headingText.length > 21 ? headingText.slice(0, 21) + '...' : headingText}
-      </span>
-      <ContainerDimensions>
-        {
-          ({ width }) =>
-            <span
-              className={localizedDescription.length > width / PANECHAR ? 'pane-subtitle-text hint--bottom hint--medium' : 'pane-subtitle-text'}
-              aria-label={localizedDescription}>
-              {
-                localizedDescription.length > width / PANECHAR ?
-                  localizedDescription.slice(0, Math.round(width / PANECHAR)) + '...' :
-                  localizedDescription
-              }
-            </span>
-        }
-      </ContainerDimensions>
-    </div>;
-  }
-
-  /**
-   * create title container content with selected justification
-   * @param {boolean} ltr - justification to use
-   * @return {*}
-   */
-  function getTitleContainer(ltr) {
-    if (ltr) {
-      return <>
-        {getTitleContainerContent(ltr)}
-        {getGlyphicon()}
-      </>;
-    } else { // arrange rtl
-      return <>
-        {getGlyphicon()}
-        {getTitleContainerContent(ltr)}
-      </>;
-    }
-  }
 
   return (
     <div className="pane-container">
       <div className="pane-title-container">
-        {getTitleContainer(layoutDirectionLTR)}
+        {getTitleContainer(layoutDirectionLTR, headingText, localizedDescription,
+          clickToRemoveResourceLabel, index, removePane)}
       </div>
       <div className={direction === 'ltr' ? 'verse-content-container-ltr' : 'verse-content-container-rtl'}>
         <Verse
