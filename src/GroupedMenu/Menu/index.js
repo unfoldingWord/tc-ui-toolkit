893b45896e43c239585b8e8aa822215ff5a331e0
Bruce McLean <mcleanb@photo-nomad.net>
2020-05-28 15:17:39 -0400

    cleanup

--- src/GroupedMenu/Menu/index.js
+++ src/GroupedMenu/Menu/index.js
@@ -275,8 +275,6 @@ class Menu extends React.Component {
       groupId,
       itemId,
     } = item;
-    console.log(`isItemSelected: active item: ${JSON.stringify(activeItem)}`);
-    console.log(`isItemSelected: item: ${JSON.stringify(item)}`);
     return (
       activeItem &&
       activeItem.groupId === groupId &&
