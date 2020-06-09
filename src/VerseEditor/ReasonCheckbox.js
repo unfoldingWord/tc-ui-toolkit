0cb9bae9fb24a6a9f98c2bd78b6c05d28058580c
mannycolon <colonmanuel7@gmail.com>
2020-06-09 15:17:59 -0400

    Fixing ReasonCheckbox

--- src/VerseEditor/ReasonCheckbox.js
+++ src/VerseEditor/ReasonCheckbox.js
@@ -1,80 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const styles = {
+const useStyles = makeStyles({
   formControlLabelRoot: { height: 30 },
   formControlLabel: {
     justifyContent: 'flex-start',
     fontWeight: 700,
     fontSize: 16,
   },
-  checkBox: { '&$checked': { color: 'var(--accent-color-dark)' } },
-  checked:{},
-};
+  checkBoxRoot: { fontSize: '24px' },
+  checkedCheckBox: { color: 'var(--accent-color-dark)' },
+});
 
-/**
- * @callback ReasonCheckbox~onCheck
- * @param {string} reason - the reason being checked
- * @param {bool} checked - indicates if the reason is checked
- */
-
-/**
- * Renders a reason checkbox
- * @property {string} reason - the reason for the edit
- * @property {string} label - the checkbox label
- * @property {string[]} selectedReasons - an array of selected reasons
- * @property {ReasonCheckbox~onCheck} onCheck - callback when the checkbox is changed
- */
-
-class ReasonCheckbox extends React.Component {
-  constructor(props) {
-    super(props);
-    this._handleCheck = this._handleCheck.bind(this);
-  }
-
-  _handleCheck(e, checked) {
-    const { reason, onCheck } = this.props;
+function ReasonCheckbox({
+  label,
+  reason,
+  onCheck,
+  selectedReasons,
+}) {
+  function handleCheck(e, checked) {
     onCheck(reason, checked);
   }
 
+  const classes = useStyles();
 
-  render() {
-    const {
-      reason, label, selectedReasons, classes,
-    } = this.props;
-    return (
-      <FormControlLabel
-        classes={{
-          root: classes.formControlLabelRoot,
-          label: classes.formControlLabel,
-        }}
-        control={
-          <Checkbox
-            classes={{
-              root: classes.checkBox,
-              checked:classes.checked,
-            }}
-            checked={selectedReasons.includes(reason)}
-            onChange={this._handleCheck}
-          />
-        }
-        label={label}
-      />
-    );
-  }
+  return (
+    <FormControlLabel
+      classes={{
+        root: classes.formControlLabelRoot,
+        label: classes.formControlLabel,
+      }}
+      control={
+        <Checkbox
+          classes={{
+            root: classes.checkBoxRoot,
+            checked: classes.checkedCheckBox,
+          }}
+          checked={selectedReasons.includes(reason)}
+          onChange={handleCheck}
+        />
+      }
+      label={label}
+    />
+  );
 }
 
 ReasonCheckbox.propTypes = {
-  reason: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
-  selectedReasons: PropTypes.arrayOf(PropTypes.string),
   onCheck: PropTypes.func.isRequired,
-  classes: PropTypes.object.isRequired,
+  reason: PropTypes.string.isRequired,
+  selectedReasons: PropTypes.arrayOf(PropTypes.string),
 };
 
 ReasonCheckbox.defaultProps = { selectedReasons: [] };
 
-export default withStyles(styles)(ReasonCheckbox);
+export default ReasonCheckbox;
