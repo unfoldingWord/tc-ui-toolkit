7ac21546d7daeba77f7922b24a5cfaa51b1a296e
mannycolon <colonmanuel7@gmail.com>
2020-06-16 01:24:31 -0400

    ThreeDotIcon.js

--- src/ThreeDotIcon/ThreeDotIcon.js
+++ src/ThreeDotIcon/ThreeDotIcon.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 // import IconButton from '@material-ui/core/IconButton';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Glyphicon } from 'react-bootstrap';
 
-function ThreeDotIcon({ onClick, style }) {
+function ThreeDotIcon({
+  onClick, style, title,
+}) {
   return (
+    <Glyphicon
+      glyph="option-vertical"
+      title={title}
+      onClick={onClick}
+      style={{ padding: 0, ...style }}
+    />
     // <IconButton
     //   aria-label="more"
     //   aria-controls="long-menu"
@@ -12,12 +21,13 @@ function ThreeDotIcon({ onClick, style }) {
     //   onClick={onClick}
     //   style={{ padding: 0, ...style }}
     // >
-    <MoreVertIcon onClick={onClick} style={{ padding: 0, ...style }}/>
+    // <MoreVertIcon onClick={onClick} style={{ padding: 0, ...style }}/>
     // </IconButton>
   );
 }
 
 ThreeDotIcon.propTypes = {
+  title: PropTypes.string,
   onClick: PropTypes.func.isRequired,
   style: PropTypes.object.isRequired,
 };
