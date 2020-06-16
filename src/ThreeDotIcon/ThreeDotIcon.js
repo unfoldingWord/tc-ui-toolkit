1e0980e1e05d0d80037aebe8aa17bb1f888686dd
mannycolon <colonmanuel7@gmail.com>
2020-06-16 01:07:42 -0400

    f

--- src/ThreeDotIcon/ThreeDotIcon.js
+++ src/ThreeDotIcon/ThreeDotIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
+// import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function ThreeDotIcon({ onClick, style }) {
