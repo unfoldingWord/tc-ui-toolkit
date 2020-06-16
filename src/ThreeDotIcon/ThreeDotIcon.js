0c1424f02d7a5829a4cbc1f16ebb140726698b95
mannycolon <colonmanuel7@gmail.com>
2020-06-16 00:34:42 -0400

    Created ThreeDotIcon component

---
+++ src/ThreeDotIcon/ThreeDotIcon.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+function ThreeDotIcon({ onClick, style }) {
+  return (
+    <IconButton
+      aria-label="more"
+      aria-controls="long-menu"
+      aria-haspopup="true"
+      onClick={onClick}
+      style={{ padding: 0, ...style }}
+    >
+      <MoreVertIcon />
+    </IconButton>
+  );
+}
+
+ThreeDotIcon.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.object.isRequired,
+};
+
+export default ThreeDotIcon;
--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ export { default as Bookmark } from './Bookmark';
 export { default as WordLexiconDetails } from './WordLexiconDetails';
 export { default as SpinningLogo } from './SpinningLogo';
 export { default as FontSizeSlider } from './FontSizeSlider';
+export { default as ThreeDotIcon } from './ThreeDotIcon';
 export { default as DropdownMenu, MenuItem } from './DropdownMenu';
 export { getAlignedText } from './VerseCheck/helpers/checkAreaHelpers';
 export {
