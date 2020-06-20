7160b1b3eb541178f8984deccbfdd7158bb614a6
mannycolon <colonmanuel7@gmail.com>
2020-06-20 00:20:41 -0400

    FontSizeSlider.js

--- src/FontSizeSlider/FontSizeSlider.js
+++ src/FontSizeSlider/FontSizeSlider.js
@@ -8,12 +8,12 @@ import Tooltip from '@material-ui/core/Tooltip';
 
 const LightTooltip = withStyles((theme) => ({
   tooltip: {
-    backgroundColor: theme.palette.common.white,
-    color: 'rgba(0, 0, 0, 0.87)',
+    // backgroundColor: ,
+    color: theme.palette.common.white,
     boxShadow: theme.shadows[1],
     fontSize: 11,
   },
-  arrow: { backgroundColor: theme.palette.common.white },
+  // arrow: { backgroundColor: theme.palette.common.white },
 }))(Tooltip);
 
 function ValueLabelComponent(props) {
