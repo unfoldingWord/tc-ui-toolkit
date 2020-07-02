6c1094a4ba1224880e07ca49720533cff9f4f129
Bruce McLean <mcleanb@photo-nomad.net>
2020-07-02 18:03:02 -0400

    tweak tooltip width calculations

--- src/GroupedMenu/Menu/MenuItem.js
+++ src/GroupedMenu/Menu/MenuItem.js
@@ -182,16 +182,12 @@ class MenuItem extends React.Component {
    * Check for the tooltip text overflow
    */
   checkOverflow = () => {
-    const { direction } = this.props;
-    const rtl = !isLTR(direction);
-    const padding = rtl ? 20 : 8; // correct for padding width
-
     delay(500).then(() => { // after screen renders check sizing
-      const { title } = this.props;
+      const { direction } = this.props;
+      const padding = isLTR(direction) ? 8 : 20; // correct for padding width
       const overflow =
         this.listItemTextRef.current.offsetWidth <
         this.textRef.current.offsetWidth + padding;
-      console.log(`checkOverflow(${title.substr(0,5)}) Overflow ${overflow}), listItemTextRef ${this.listItemTextRef.current.offsetWidth}, textRef ${this.textRef.current.offsetWidth}, padding ${padding}`);
 
       if (overflow !== this.state.overflow) {
         this.setState({ overflow });
