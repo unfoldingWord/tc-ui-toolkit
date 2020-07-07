fef9da128b9e1f8b4fe353ebe05bd4f11b116eb9
mannycolon <colonmanuel7@gmail.com>
2020-07-07 10:07:50 -0400

    Fixing expanded scripture pane

--- src/ScripturePane/ExpandedScripturePaneModal/ChapterView/VerseRow/index.js
+++ src/ScripturePane/ExpandedScripturePaneModal/ChapterView/VerseRow/index.js
@@ -88,6 +88,7 @@ class VerseRow extends Component {
                 translate={translate}
                 verseText={verseText}
                 direction={direction}
+                languageId={languageId}
                 onEdit={this.handleEdit}
                 verse={currentVerseNumber}
                 verseElements={verseElements}
